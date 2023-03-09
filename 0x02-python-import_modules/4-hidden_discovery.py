f246abba44e09b31253549b369b773a9be0af046
Daniel930603 <117520870+Daniel930603@users.noreply.github.com>
2023-03-09 15:29:22 +0300

    Task 4

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
+    import sys
     import hidden_4
-
-    prf = "__"
-    for x in dir(hidden_4):
-        if prf not in x:
-            print(x)
+    for n in dir(hidden_4):
+        if n[:2] != "__":
+            print(n)
