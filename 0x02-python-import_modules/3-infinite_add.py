cdb56e7140b18f121dc2748e601f5f00b3fd0a57
Daniel930603 <117520870+Daniel930603@users.noreply.github.com>
2023-03-09 15:26:51 +0300

    Task 3

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import sys
-
-    n = len(sys.argv)
-
-    sum = 0
-
-    for i in range(1, n):
-        sum += int(sys.argv[i])
-    print(f"{sum}")
+    argv = sys.argv[1:]
+    argv_count = len(argv)
+    index = 1
+    res = 0
+    while index <= argv_count:
+        res += int(sys.argv[index])
+        index += 1
+    print("{:d}".format(res))
