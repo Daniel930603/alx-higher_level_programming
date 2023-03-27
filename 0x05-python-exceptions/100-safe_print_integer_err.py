fe8d529e506e20955a91c9bfc68e1b8368822b79
Daniel930603 <117520870+Daniel930603@users.noreply.github.com>
2023-03-27 23:42:43 +0300

    Task 7

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -1,16 +1,19 @@
 #!/usr/bin/python3
-import traceback as tb
+import sys
 
 def safe_print_integer_err(value):
-    ret = False
+    """Prints an integer with "{:d}".format().
+    If a ValueError message is caught, a corresponding
+    message is printed to standard error.
+    Args:
+        value (int): The integer to print.
+    Returns:
+        If a TypeError or ValueError occurs - False.
+        Otherwise - True.
+    """
     try:
         print("{:d}".format(value))
-        ret = True
-    except (ValueError, TypeError) as message:
-        tb.print_exception(Exception,
-                           Exception(message),
-                           None,
-                           limit=0,
-                           chain=False)
-    finally:
-        return ret
+        return (True)
+    except (TypeError, ValueError):
+        print("Exception: {}".format(sys.exc_info()[1]), file=sys.stderr)
+        return (False)
